dfb1290805e14f2d993dddeea4d525d1e14c616d
Kate Drwęcka <katedrwecka@gmail.com>
2026-03-17 11:09:30 -0400

    Remove lever references, job form Since we're no longer using lever I've removed all references to it. Also removed the "contact form" (really a job post form), both from the html pages and from the js.

--- assets/js/cds-app.js
+++ assets/js/cds-app.js
@@ -1,6 +1,3 @@
-import * as params from '@params';
-const endpoint = params.lever_api_endpoint;
-
 Object.defineProperty(HTMLMediaElement.prototype, "playing", {
   get: function () {
     return !!(
@@ -23,69 +20,5 @@ $(document).ready(function () {
       "data-gc-analytics-navigation",
       `footer:Canadian Digital Service: ${e.detail}`
     );
-  });
-
-  /**
-   * Contact Form Submit
-   */
-
-  $("#contactForm").submit(function (event) {
-    event.preventDefault();
-
-    setTimeout(() => {
-      var valid = true,
-        errors = [];
-
-      $("#contactForm .validate-required").each(function () {
-        var $this = $(this);
-        if ($this[0].errorMessage) {
-          errors.push($this[0]);
-          valid = false;
-        }
-      });
-
-      if (!valid) {
-        errors[0].focus();
-        return false;
-      }
-
-      /**
-       * Disable button to prevent double-submitting
-       */
-      $("#submit-btn").toggleClass("loading");
-      $("#submit-btn").toggleClass("inactive");
-      $("#submit-btn").attr("disabled", true);
-      $("#job-submit-btn").attr("disabled", true);
-
-      var formData = new FormData($("#contactForm")[0]);
-      var pageLanguage = $("html").attr("lang");
-
-      $.ajax({
-        type: "POST",
-        url: endpoint,
-        data: formData,
-        contentType: false,
-        processData: false,
-        complete: function (r) {
-          console.log(r.responseText);
-        },
-        success: function () {
-          if (pageLanguage == "en") {
-            window.location.href = "/thank-you/";
-          } else {
-            window.location.href = "/merci/";
-          }
-        },
-        error: function (xhr, textStatus, errorThrown) {
-          console.log("Error", textStatus);
-          console.log("Error", errorThrown);
-          if (pageLanguage == "en") {
-            window.location.href = "/error/";
-          } else {
-            window.location.href = "/erreur/";
-          }
-        },
-      });
-    }, 200);
   });
 });
